657b475918e6b925a0da449ada5e1ecc3b09ab92
Siu Sin <nipsiusin900@gmail.com>
2025-08-14 17:27:32 +0800

    add TAGS

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,6 +1,3 @@
-#include "CarFactory.h"
-#include "Logger.h"
-#include "TheOrigin.h"
 #include <array>
 #include <compare>
 #include <cstddef>
@@ -9,16 +6,22 @@
 #include <optional>
 #include <print>
 #include <utility>
+#include <string>
+#include <format>
+
+#include "CarFactory.h"
+#include "Logger.h"
+#include "TheOrigin.h"
 
 using namespace std;
 
 namespace mycode {
 void foo() { std::println("foo() from mycode namespace."); }
 namespace inner_space {
-int r;
-namespace inner_er_space {
-int j;
-}
+    int r;
+    namespace inner_er_space {
+        int j;
+    }
 } // namespace inner_space
 
 } // namespace mycode
@@ -28,72 +31,96 @@ using namespace mycode;
 optional<int> getData(bool giveIt)
 
 {
-  if (giveIt) {
-    return 42;
-  }
-  return nullopt;
+    if (giveIt) {
+        return 42;
+    }
+    return nullopt;
 }
 
-int main() {
+int main()
+{
+    unsigned uj { 5U };
+    unsigned 中文 {5U };
+    TheOrigin myOrigin;
+    ToyotaFactory myFactory;
+    auto myCar { myFactory.requestCar() };
+println("こんにちは世界");
+    auto b = { 11, 22 };
+    println("{}", myCar->info());
+    Logger::setLogFilename("log.out");
+    std::println("Hueta?");
+    std::println("{}", 中文);
 
-  unsigned uj{5U};
-  TheOrigin myOrigin;
-  ToyotaFactory myFactory;
-  auto myCar{myFactory.requestCar()};
+    mycode::foo();
+    foo();
 
-  auto b = {11, 22};
-  println("{}", myCar->info());
-  Logger::setLogFilename("log.out");
-  std::println("Hueta?");
+    myOrigin.Attribute = true;
+    println("myOrigin.Attrbute = {} ", myOrigin.Attribute);
+    //  myOrigin.ShowTheRealAttribute();
 
-  mycode::foo();
-  foo();
+    int i { 11 };
+    strong_ordering result { i <=> 0 };
 
-  myOrigin.Attribute = true;
-  println("myOrigin.Attrbute = {} ", myOrigin.Attribute);
-  //  myOrigin.ShowTheRealAttribute();
-
-  int i{11};
-  strong_ordering result{i <=> 0};
-
-  if (result == strong_ordering::less) {
-    println("less");
-  }
-  if (result == strong_ordering::greater) {
-    println("greater");
-  }
-  if (result == strong_ordering::equal) {
-    println("equal");
-  }
-
-  array<int, 3> arr{9, 8, 7};
-  println("Array = {}", arr);
-  println("Array size = {}", arr.size());
-  println("Array 2nd element = {}", arr[1]);
-
-  pair<double, int> myPair{1.23, 5};
-  pair<char, int> myPair2{'c', 8};
-  println("my pair = {}", myPair);
-  println("my pair2 = {}", myPair2);
-
-  optional<int> data1{getData(true)};
-  optional<int> data2{getData(false)};
-
-  if (data1) {
-    println("data1.has_value = {}", data1.has_value());
-    println("data1.value = {}", data1.value());
-  }
-
-  println("data2.has_value = {}", data2.has_value());
-  try {
-    println("data2.value = {}", data2.value());
-  } catch (const bad_optional_access &ex) {
-    try {
-      println("in 1st catch data2.value = {}", data2.value());
-    } catch (const bad_optional_access &ex2) {
-      println(" in 2nd catch Fuck up excepton: {}", ex2.what());
+    if (result == strong_ordering::less) {
+        println("less");
     }
-  }
+    if (result == strong_ordering::greater) {
+        println("greater");
+    }
+    if (result == strong_ordering::equal) {
+        println("equal");
+    }
 
-  return 0;
+    array<int, 3> arr { 9, 8, 7 };
+    println("Array = {}", arr);
+    println("Array size = {}", arr.size());
+    println("Array 2nd element = {}", arr[1]);
+
+    pair<double, int> myPair { 1.23, 5 };
+    pair<char, int> myPair2 { 'c', 8 };
+    println("my pair = {}", myPair);
+    println("my pair2 = {}", myPair2);
+
+    optional<int> data1 { getData(true) };
+    optional<int> data2 { getData(false) };
+
+    if (data1) {
+        println("data1.has_value = {}", data1.has_value());
+        println("data1.value = {}", data1.value());
+    }
+
+    println("data2.has_value = {}", data2.has_value());
+    try {
+        println("data2.value = {}", data2.value());
+    } catch (const bad_optional_access& ex) {
+        try {
+            println("in 1st catch data2.value = {}", data2.value());
+        } catch (const bad_optional_access& ex2) {
+            println(" in 2nd catch Fuck up excepton: {}", ex2.what());
+        }
+    }
+      int x{ 42 };
+        int y{ 84 };
+
+        // C-style function.
+        printf("x has value %d and y has value %d.\n", x, y);
+
+        // C++ I/O stream.
+        cout << "x has value " << x << " and y has value " << y << '.' << endl;
+
+        // C++20 std::format() + I/O streams.
+        cout << format("x has value {} and y has value {}.", x, y) << endl;
+
+        // C++23 std::println().
+        println("x has value {} and y has value {}.", x, y);
+
+        // C++23 std::println() + Unicode.
+        println("こんにちは世界");
+
+        // I/O streams + Unicode.
+        cout << "こんにちは世界" << endl;
+
+        // C++23 std::println() + Unicode emojis.
+        println("😀");
+    return 0;
 }
